03836bfd3950fccd09b89cf4b3f907d146d395d4
jonathan velez parra <youremail@example.com>
2023-09-26 19:49:15 -0500

    first version of the proyect

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Normalice.css">
+    <link rel="stylesheet" href="assets/custom.css">
+    
+    <title>Trendy commerce</title>
+</head>
+<body>
+    <!--Navbar-->
+<div class="nav container-fluid d-flex justify-content-between align-items-center">
+    <div><img src="img/Logo.png" alt="" width="168px"></div>
+    <div class="d-flex align-items-center search-bar">
+        <div class="w-auto flex-fill position-relative">
+            <img class="search-icon" src="img/Nav-bar/lupa (1) 1.png" alt="" width="25px">
+            <input type="text" class="form-control rounded-pill seacrh-input" id="formGroupExampleInput" placeholder="Search">
+        </div>
+        <div id="shop-cart" class="shop-cart position-relative"><img src="img/Nav-bar/Recurso 16-8.png" alt="" height="30px">
+            <div class="count-products">
+						<span id="contador-productos">0</span>
+					</div>
+            </div>
+    </div>
+    <div class="sign-and-menu-container d-flex align-items-center justify-content-center">
+        <div>
+            <a href="" class="btn btn-light rounded-pill login-singup border border-light-subtle">Sign up</a>
+        </div>
+            <div>
+                <a href="" class="btn btn-light rounded-pill d-flex flex-row-reverse login-singup align-items-center border border-light-subtle"><img src="img/Nav-bar/Recurso 2-8.png" alt="" width="16px" height="22px"> Log in</a>
+            </div>
+                <div class="main-menu">
+                    <img src="img/Nav-bar/Recurso 3-8 1.png" width="28.543px" alt="">
+                </div>
+    </div>
+</div>
+
+
+
+
+
+
+    <?php
+    echo "Hola";
+    ?>
+<script src="">
+</script>
+</body>
+</html>
