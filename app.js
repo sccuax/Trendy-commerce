e1e12286ef1cb87afb673e2c799642fb4b03842f
jonathan velez parra <youremail@example.com>
2023-11-03 00:31:00 -0500

    s

--- app.js
+++ app.js
@@ -1,73 +1,127 @@
 'use strict';
 
 //-------------------------------Cart Dropdown
-const cartDropdow = document.querySelector('#shop-cart');
+const cartDropdown = document.querySelector('#shop-cart');
 const showCart = document.querySelector('.cart-dropdown');
 const emptyCart = document.querySelector('.shop-cart-dropdown');
 const counterProducts = document.querySelector('#contador-producto');
-const cartTotal= document.querySelector('.bill');
+const cartTotal = document.querySelector('.bill');
 const coursesList = document.querySelector('#courses-list');
-const rowProduct = document.querySelector('.cards_t');
-let conterTotal;
-let totalbill = 0;
+const ProductHorizontal = document.querySelector('.container_cards_rows');
+let counterTotal;
+let totalBill = 0;
 let cart = [];
 
-cartDropdow.addEventListener('click', showCatDropdown)
-function showCatDropdown(){
-    if(showCart.classList.contains('hidden')){
+cartDropdown.addEventListener('click', showCatDropdown);
+
+function showCatDropdown() {
+    if (showCart.classList.contains('hidden')) {
         showCart.classList.remove('hidden');
-        showCart.classList.add('vissible');
-    }else{
+        showCart.classList.add('visible');
+    } else {
         showCart.classList.add('hidden');
-        showCart.classList.remove('vissible');
+        showCart.classList.remove('visible');
     }
 }
- 
-//add products to the cart
-coursesList.addEventListener('click', addCourse);
 
-function addCourse(e){
-    e.preventDefault();
-    if(e.target.classList.contains('add-this-product')){
+// Add products to the cart
+/* ProductHorizontal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('add-this-product')) {
         const selectedCourse = e.target.parentElement.parentElement;
-        readInfoCourse(selectedCourse);
+        readInfoProduct(selectedCourse);
+    }
+});
+
+coursesList.addEventListener('click', addProduct);
+
+function addProduct(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('add-this-product')) {
+        const selectedCourse = e.target.parentElement.parentElement;
+        readInfoProduct(selectedCourse);
     }
 }
 
-function readInfoCourse(course){
-
+function readInfoProduct(product) {
     const infoCourse = {
-        image: course.querySelector('img').src,
-        tittle: course.querySelector('p').textContent,
-        typeOfProduct: course.querySelector('type_product_cart-dropdown').textContent,
-        price: course.  querySelector('.price-cart-dropdown').textContent,
+        image: product.querySelector('img').src,
+        title: product.querySelector('.card_text_profuct-name').textContent,
+        typeOfProduct: product.querySelector('.card_text_type').textContent,
+        price: product.querySelector('.buy_card_button').textContent,
         quantity: 1,
-        id: course.querySelector('button').getAttribute('data-id'),
+        id: product.querySelector('.add-this-product').getAttribute('data-id'),
     };
 
-    const alreadyExist = cart.some((course) => course.id === infoCourse.id);
-        if(alreadyExist){
-            cart = cart.map((course) => {
-                if(course.id === infoCourse.id){
-                    course.quantity++;
-                    return course;
-                }else{
-                    return course;
-                }
-            });
-        }else{
-            cart.push(infoCourse);
-        }
-        showHTML();
+    const alreadyExist = cart.some((product) => product.id === infoCourse.id);
+    if (alreadyExist) {
+        cart = cart.map((product) => {
+            if (product.id === infoCourse.id) {
+                product.quantity++;
+                return product;
+            } else {
+                return product;
+            }
+        });
+    } else {
+        cart.push(infoCourse);
+    }
+    showHTML();
 }
 
-function showHTML(){
-if (!cart.length){
-    cart
+function showHTML() {
+    cleanCart();
+
+    cart.forEach((product) => {
+        const containerProduct = document.createElement('div');
+        containerProduct.classList.add('container-cart-product');
+
+        containerProduct.innerHTML = `
+        <div class="hidden cart-dropdown flex-column justify-content-between position-absolute align-items-center">
+        <div class="container-cart-product justify-content-between align-content-center align-self-stretch d-flex">
+            <img class="img-cart-container" src="img/cards_products/fosilClassic.jpg" alt="">
+            <div class="info-cart-container d-flex flex-column align-items-center align-self-stretch">
+                <div class="info-cart-container-text d-flex justify-content-between align-items-start align-content-start align-self-stretch flex-wrap">
+                    <p class="name-product_cart-dropdown">Fosil C-automatic</p>
+                    <p class="type_product_cart-dropdown">Automatic watch</p>
+                    <img class="" src="img/boton-x.png" alt="" width="16px">
+                </div>
+                <div class="cart-container-price-buttons d-flex justify-content-between align-items-center align-self-stretch">
+                    <p class="price-cart-dropdown">$100</p>
+                    <div class="container_counter-cards position-relative cart-container_counters d-flex align-items-center">
+                        <button class="count-m d-flex align-items-center justify-content-center">-</button>
+                        <span class="counter_cards d-flex align-items-center justify-content-center">0</span>
+                        <button class="count-m d-flex align-items-center justify-content-center">+</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="text-cart-dropdown">Empty</p>
+        <a class="shop-cart-dropdown d-flex justify-content-center align-items-center" href="">Shop now</a>
+    </div>
+        `;
+        ProductHorizontal.appendChild(containerProduct);
+        counterSpan();
+        totalAmount();
+    });
 }
+
+function counterSpan() {
+    let counterTotal = 0;
+    cart.forEach((product) => {
+        counterTotal += product.quantity;
+    });
+    counterProducts.innerText = counterTotal;
 }
 
+function cleanCart() {
+    ProductHorizontal.innerHTML = '';
+}
 
-
-
-
+function totalAmount() {
+    let totalBill = 0;
+    cart.forEach((product) => {
+        totalBill += parseInt(product.price.slice(1)) * product.quantity;
+    });
+    cartTotal.innerHTML = `$${totalBill.toLocaleString()}`;
+}
+ */
---
+++ cart.js
@@ -0,0 +1,104 @@
+// Variables de elementos del DOM
+const emptyCart = document.querySelector('.shop-cart-dropdown');
+const counterProducts = document.querySelector('#contador-producto');
+const addProduct = document.querySelector('.container_cards_rows');
+const cartEmpty = document.querySelector('.text-cart-dropdown');
+const rowProduct = document.querySelector('.container-javascript');
+const ConuterProducts = document.querySelector('#contador-productos');
+const ProductHorizontal = document.querySelector('.container_cards_rows');
+let totalBill = 0;
+let cart = [];
+
+// Agregar productos al carrito al hacer clic en el botón "+"
+
+addProduct.addEventListener('click', getProduct);
+
+function getProduct(){
+    e.preventDefault();
+    if (e.target.classList.contains('add-this-product')){
+        const selectedProduct = e.target.parentElement.parentElement;
+        infoProduct(selectedProduct);
+        showHTML();
+    }
+}
+
+counterProducts.addEventListener('click', counterSpan);
+
+function infoProduct(product){
+    const item = {
+        image: product.querySelector('img').src,
+        name: product.querySelector('.card_text_profuct-name').textContent,
+        type: product.querySelector('.card_text_type').textContent,
+        price: product.querySelector('.buy_card_button').textContent,
+        quantity: 1,
+        id: coursesList.querySelector('.add-this-product').getAttribute('data-id'),
+    };
+
+    const alreadyExist = cart.some((product) => product.id === item.id);
+    if (alreadyExist){
+        cart = cart.map((product) =>{
+            if(product.id === item.id){
+                product.quantity;
+                return product;
+            }else{
+                return product;
+            }
+        });
+    }else{
+        cart.push(item);
+    }
+    showHTML();
+}
+
+function showHTML(){
+    if(!cart.length){
+        cartEmpty.classList.remove('hidden');
+        rowProduct.classList.add('hidden');
+    }else{
+        cartEmpty.classList.add('hidden');
+        rowProduct.classList.remove('hidden');
+    }
+
+    cleanCart();
+
+    cart.forEach((product) => {
+        const containerProduct = document.createElement('div');
+        containerProduct.classList.add('container-cart-product justify-content-between align-content-center align-self-stretch d-flex');
+
+        containerProduct.innerHTML = `
+        <img class="img-cart-container" src="${product.image}" alt="">
+        <div class="info-cart-container d-flex flex-column align-items-center align-self-stretch">
+            <div class="info-cart-container-text d-flex justify-content-between align-items-start align-content-start align-self-stretch flex-wrap">
+                <p class="name-product_cart-dropdown">${product.name}</p>
+                <p class="type_product_cart-dropdown">${product.type}</p>
+                <img class="" src="img/boton-x.png" alt="" width="16px" data-id="${product.id}>
+            </div>
+            <div class="cart-container-price-buttons d-flex justify-content-between align-items-center align-self-stretch">
+                <p class="price-cart-dropdown">${product.price}</p>
+                <div class="container_counter-cards position-relative cart-container_counters d-flex align-items-center">
+                    <button class="count-m d-flex align-items-center justify-content-center">-</button>
+                    <span class="counter_cards d-flex align-items-center justify-content-center">${product.quantity}</span>
+                    <button class="count-m d-flex align-items-center justify-content-center">+</button>
+                </div>
+            </div>
+        </div>
+        `;
+        rowProduct.appendChild(containerProduct);
+        counterSpan();
+    });
+}
+
+function counterSpan() {
+    counterTotal = 0;
+    cart.forEach((product) => {
+        counterTotal += product.quantity;
+    });
+    ConuterProducts.innerText = counterTotal;
+  }
+
+  function cleanCart() {
+    rowProduct.innerHTML = '';
+}
+
+
+console.log("hola");
